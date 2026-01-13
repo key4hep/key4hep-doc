41ffa4e06e3500935dc25a1c353928e66b696a5a
Thomas Madlener <thomas.madlener@desy.de>
2026-01-13 13:36:12 +0100

    Move non common code back to specific script

--- .github/scripts/fetch_external_sources.sh
+++ .github/scripts/fetch_external_sources.sh
@@ -33,16 +33,52 @@ for arg in "$@"; do
   esac
 done
 
-
-# Default GitHub organizations to try when fetching external files
-GITHUB_ORGS="key4hep HEP-FCC AIDASoft iLCSoft"
-FETCH_BRANCHES="main master"
-
 # NOTE: Fetch this late so that setting the customizations for the functions can
 # take place
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/utilities.sh"
 
+# process one markdown file with content that potentially needs fetching from an
+# external repository
+fetch_for_file() {
+  local file_to_proc=${1}
+  local file_dir=$(dirname $(realpath ${file_to_proc}))
+
+  echo "Fetching external contents for file '${file_to_proc}'"
+
+  while read -r line; do
+    # Check if line is non-empty and ends on .md
+    if [ -n "${line}" ] && [[ "${line}" == *.md ]] || [[ "${line}" == *.png ]]; then
+      # If the file exists do nothing, otherwise pull it in from github
+      local file_to_fetch=${file_dir}/${line}
+      if [ "${FORCE}" = true ]; then
+        echo "Force option enabled. Trying to fetch '${line}' from github"
+      elif ! ls "${file_to_fetch}" > /dev/null 2>&1; then
+        echo "${line} does not exist. Trying to fetch it from github"
+      else
+        continue
+      fi
+
+      local outputdir=$(dirname ${file_to_fetch})
+      mkdir -p ${outputdir}  # make the directory for the output
+
+      # Try a few github organizations
+      for org in key4hep HEP-FCC AIDASoft iLCSoft; do
+        echo "Trying to fetch from github organization: '${org}'"
+        if try_fetch ${org} ${line} ${file_dir}; then
+          echo "Fetched successfully from organization '${org}'"
+          break
+        fi
+      done
+
+      # Check again if we have successfully fetched the file
+      if ! ls "${file_to_fetch}" > /dev/null 2>&1; then
+        echo "Could not fetch file '${line}' from external sources" 1>&2
+        exit 1
+      fi
+    fi
+  done < ${file_to_proc}
+}
 
 fetch_for_file docs/index.md
 fetch_for_file docs/tutorials/README.md
